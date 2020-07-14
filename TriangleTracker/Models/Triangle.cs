3a8142eb26bea8cb43772c5ec0d78c52de3539e6
Taylor Somers <tsomers@yandex.com>
2020-07-14 14:36:59 -0400

    Add test-passing logic to IsTriangle method
    Co-authored-by: Evgeniya Chernaya <evgenya.chernaya@gmail.com>
    Co-authored-by: Cody Fritz <clanalia55@gmail.com>
    Co-authored-by: Johnny Duverseau <duverseaujohnny21@gmail.com>

--- TriangleTracker/Models/Triangle.cs
+++ TriangleTracker/Models/Triangle.cs
@@ -1,10 +1,54 @@
 using System;
 using System.Collections.Generic; //remove if not using Dictionary in app //
 
-namespace NamespaceName
+namespace TriangleTracker
 {
-  class ClassName
+  public class Triangle
   {
-    
+    public int Side1 {get; set;}
+    public int Side2 {get; set;}
+    public int Side3 {get; set;}
+
+    public Triangle(int side1, int side2, int side3)
+    {
+      Side1 = side1;
+      Side2 = side2;
+      Side3 = side3;
+    }
+
+    public bool IsEquilateral()
+    {
+      if(Side1 == Side2 && Side2 == Side3)
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+    }
+    public bool IsIsosceles()
+    {
+      if(Side1 == Side2 || Side2 == Side3 || Side1 == Side3)  
+      {
+        return true;
+      }
+      else 
+      {
+        return false; 
+      }      
+    }
+
+    public bool IsTriangle()
+    {
+      if (Side1 >= Side2 + Side3 || Side2 >= Side1 + Side3 || Side3 >= Side1 + Side2)
+      {
+        return false;
+      }
+      else
+      {
+        return true;
+      }
+    }
   }
 }
