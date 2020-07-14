f8febee6c7e1f26cbf6ad876daa41e82c87945b1
Taylor Somers <tsomers@yandex.com>
2020-07-14 13:23:57 -0400

    Initial commit
    Co-authored-by: Evgeniya Chernaya <evgenya.chernaya@gmail.com>
    Co-authored-by: Cody Fritz <clanalia55@gmail.com>
    Co-authored-by: Johnny Duverseau <duverseaujohnny21@gmail.com>

---
+++ TriangleTracker.Tests/ModelTests/Triangle.Tests.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TriangleTracker; // ewProject
+
+namespace TriangleTracker.Tests
+{
+  [TestClass]
+  public class BusinessLogicTests
+  {
+    [TestMethod]
+    // Test method will go here
+  }
+}
