c1b3923469b7e7e415a7e24b9612580fd530fd7e
Taylor Somers <tsomers@yandex.com>
2020-07-14 15:01:21 -0400

    Add UI logic
    Co-authored-by: Evgeniya Chernaya <evgenya.chernaya@gmail.com>
    Co-authored-by: Cody Fritz <clanalia55@gmail.com>
    Co-authored-by: Johnny Duverseau <duverseaujohnny21@gmail.com>

--- TriangleTracker/Program.cs
+++ TriangleTracker/Program.cs
@@ -1,10 +1,33 @@
 using System;
 using System.Collections.Generic; //remove if not using Dictionary in app //
-
+using TriangleTracker;
 class Program
 {
   static void Main()
   {
-    
+    Console.WriteLine("Please enter three triangle side lengths separated by spaces: ");
+    string inputString = Console.ReadLine();
+    string[] values = inputString.Split(" ");
+    int side1 = int.Parse(values[0]);
+    int side2 = int.Parse(values[1]);
+    int side3 = int.Parse(values[2]);
+    Triangle triangle = new Triangle(side1, side2, side3);
+    if (!triangle.IsTriangle())
+    {
+      Console.WriteLine("These numbers do not make a triangle.");
+    }
+    else if (triangle.IsEquilateral())
+    {
+      Console.WriteLine("This is an equilateral triangle.");
+    }
+    else if (triangle.IsIsosceles())
+    {
+      Console.WriteLine("This is an isosceles triangle.");
+    }
+    else
+    {
+      Console.WriteLine("This is a scalene triangle.");
+    }
+    Main();
   }
 }
